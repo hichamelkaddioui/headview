98b154133725be5f8c1ace8f596253e50befc38f
Hicham <hello@hicham.party>
2024-03-22 16:33:58 +0100

    feat(machine): add options dropdown

--- src/components/MachineDetail.vue
+++ src/components/MachineDetail.vue
@@ -11,27 +11,107 @@
   <div
     class="overflow-hidden rounded-lg border border-gray-300 bg-white sm:rounded-2xl"
   >
-    <div class="px-4 py-5 sm:px-6">
-      <h3 class="flex text-lg font-medium leading-6 text-gray-900">
-        <span
-          :class="[
-            machine.online ? 'bg-green-100' : 'bg-red-100',
-            'me-2 mt-0.5 inline-flex h-6 w-6 items-center justify-center rounded-full  text-sm font-semibold text-gray-800 dark:bg-gray-700 dark:text-gray-300',
-          ]"
-        >
-          <div
+    <div class="flex items-center justify-between px-4 py-5 sm:px-6">
+      <div>
+        <h3 class="flex text-lg font-medium leading-6 text-gray-900">
+          <span
             :class="[
-              machine.online ? 'bg-green-500' : 'bg-red-500',
-              'h-1.5 w-1.5 rounded-full',
+              machine.online ? 'bg-green-100' : 'bg-red-100',
+              'me-2 mt-0.5 inline-flex h-6 w-6 items-center justify-center rounded-full  text-sm font-semibold text-gray-800 dark:bg-gray-700 dark:text-gray-300',
             ]"
-          ></div>
-        </span>
+          >
+            <div
+              :class="[
+                machine.online ? 'bg-green-500' : 'bg-red-500',
+                'h-1.5 w-1.5 rounded-full',
+              ]"
+            ></div>
+          </span>
 
-        {{ machine.givenName }}
-      </h3>
-      <p class="ms-8 mt-1 max-w-2xl text-sm text-gray-500">
-        {{ machine.name }}
-      </p>
+          {{ machine.givenName }}
+        </h3>
+        <p class="ms-8 mt-1 max-w-2xl text-sm text-gray-500">
+          {{ machine.name }}
+        </p>
+      </div>
+      <div>
+        <Menu as="div" class="relative -ml-px block">
+          <div>
+            <MenuButton
+              class="inline-flex w-full justify-center rounded-xl border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-700 hover:bg-gray-50 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-1 focus:ring-indigo-500"
+            >
+              Options
+              <ChevronDownIcon class="-mr-1 ml-2 h-5 w-5" aria-hidden="true" />
+            </MenuButton>
+          </div>
+
+          <transition
+            enter-active-class="transition ease-out duration-100"
+            enter-from-class="transform opacity-0 scale-95"
+            enter-to-class="transform opacity-100 scale-100"
+            leave-active-class="transition ease-in duration-75"
+            leave-from-class="transform opacity-100 scale-100"
+            leave-to-class="transform opacity-0 scale-95"
+          >
+            <MenuItems
+              class="absolute right-0 z-30 mt-2 w-56 origin-top-right divide-y divide-gray-200 rounded-md bg-white shadow-sm ring-1 ring-black ring-opacity-20 focus:outline-none"
+            >
+              <div class="py-1">
+                <MenuItem v-slot="{ active }">
+                  <a
+                    href="#"
+                    :class="[
+                      active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
+                      'group flex items-center px-4 py-2 text-sm',
+                    ]"
+                    @click="$emit('rename')"
+                  >
+                    <PencilSquareIcon
+                      class="mr-3 h-5 w-5 text-gray-500 group-hover:text-gray-600"
+                      aria-hidden="true"
+                    />
+                    Rename machine
+                  </a>
+                </MenuItem>
+                <MenuItem v-slot="{ active }">
+                  <a
+                    href="#"
+                    :class="[
+                      active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
+                      'group flex items-center px-4 py-2 text-sm',
+                    ]"
+                    @click="$emit('create-key')"
+                  >
+                    <UserIcon
+                      class="mr-3 h-5 w-5 text-gray-500 group-hover:text-gray-600"
+                      aria-hidden="true"
+                    />
+                    Change owner
+                  </a>
+                </MenuItem>
+              </div>
+              <div class="py-1">
+                <MenuItem v-slot="{ active }">
+                  <a
+                    href="#"
+                    :class="[
+                      active ? 'bg-gray-100 text-red-600' : 'text-red-500',
+                      'group flex items-center px-4 py-2 text-sm font-medium',
+                    ]"
+                    @click="$emit('delete')"
+                  >
+                    <TrashIcon
+                      class="mr-3 h-5 w-5 text-red-500 group-hover:text-red-600"
+                      aria-hidden="true"
+                    />
+                    Delete user
+                  </a>
+                </MenuItem>
+              </div>
+            </MenuItems>
+          </transition>
+        </Menu>
+      </div>
     </div>
     <div class="overflow-x-auto border-t border-gray-200 px-4 py-5 sm:p-0">
       <dl class="sm:divide-y sm:divide-gray-200">
@@ -155,7 +235,15 @@
 
 <script setup lang="ts">
 import { useRoute } from "vue-router";
-import { ClipboardIcon, CheckIcon } from "@heroicons/vue/24/outline";
+import { Menu, MenuButton, MenuItem, MenuItems } from "@headlessui/vue";
+import {
+  ChevronDownIcon,
+  TrashIcon,
+  UserIcon,
+  ClipboardIcon,
+  CheckIcon,
+  PencilSquareIcon,
+} from "@heroicons/vue/24/outline";
 import { UseClipboard } from "@vueuse/components";
 import { machineFactory } from "../helpers/data";
 import CodeBlock from "./CodeBlock.vue";
