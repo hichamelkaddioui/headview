dc66bca9f050039eede5bd2679360850855b558c
Hicham <hello@hicham.party>
2024-03-23 02:20:38 +0100

    feat(api): implement user lists

--- src/components/UsersView.vue
+++ src/components/UsersView.vue
@@ -10,24 +10,24 @@
 
     <div class="rounded-lg border border-gray-300 bg-white sm:rounded-2xl">
       <ul role="list" class="-my-5 divide-y divide-gray-200 p-6">
-        <li v-for="person in users" :key="person.id" class="py-4">
+        <li v-for="user in users" :key="user.id" class="py-4">
           <div class="flex items-center space-x-4">
             <div class="min-w-0 flex-1">
               <p class="truncate text-sm font-medium text-gray-900">
-                {{ person.name }}
+                {{ user.name }}
               </p>
               <p class="mt-2 truncate text-sm text-gray-500">
-                {{ new Date(person.createdAt).toLocaleString() }}
+                {{ new Date(user.createdAt).toLocaleString() }}
               </p>
             </div>
 
             <div>
               <UserButtons
-                :key="person.id"
-                @keys="showPreAuthKeys(person.id)"
-                @rename="renameUser(person.id)"
-                @create-key="createPreAuthKey(person.id)"
-                @delete="deleteUser(person.id)"
+                :key="user.id"
+                @keys="showPreAuthKeys(user.id)"
+                @rename="renameUser(user.id)"
+                @create-key="createPreAuthKey(user.id)"
+                @delete="deleteUser(user.id)"
               />
             </div>
           </div>
@@ -38,12 +38,25 @@
 </template>
 
 <script setup lang="ts">
+import { computed } from "vue";
 import { UserPlusIcon } from "@heroicons/vue/24/outline";
-import { userFactory } from "../helpers/data";
+import { useAsyncState } from "@vueuse/core";
+import { api, initState } from "../plugins/api";
+import { components } from "../plugins/api/types";
 import AppLayout from "./AppLayout.vue";
 import UserButtons from "./UserButtons.vue";
 
-const users = [...new Array(3)].map(userFactory);
+const { state } = useAsyncState(api().GET("/api/v1/user"), initState);
+
+const users = computed(() => {
+  const users = state.value.data?.users;
+
+  if (!users) {
+    return [];
+  }
+
+  return users as Array<Required<components["schemas"]["v1User"]>>;
+});
 
 const showPreAuthKeys = (id: string) =>
   console.log(`Pre auth keys for user ${id}`);
