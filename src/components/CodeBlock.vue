553edc9faae2a4b6853fd8d1a8efd7762e9094b0
Hicham <hello@hicham.party>
2024-05-26 14:22:38 +0200

    fix: use proper class on code block

--- src/components/CodeBlock.vue
+++ src/components/CodeBlock.vue
@@ -1,6 +1,6 @@
 <template>
   <span
-    class="me-2 rounded-lg border border-gray-300 bg-gray-50 px-2 py-1 text-gray-800 dark:border-gray-800 dark:bg-slate-600 dark:text-slate-100"
+    class="me-2 rounded-lg border border-gray-300 bg-gray-50 px-2 py-1 text-gray-800 hover:cursor-pointer dark:border-gray-800 dark:bg-slate-600 dark:text-slate-100"
   >
     <code>{{ text }}</code>
   </span>
